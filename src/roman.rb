4b23780f415f53af55ec75a86cc7786b86184f72
Matthew John Lapehn <mlapehn@gmail.com>
2016-07-27 08:31:47 -0600

    HW

--- src/roman.rb
+++ src/roman.rb
@@ -1,3 +1,35 @@
-def romanize(arabic_numeral)
+NUMBERS = {
+   	1000 => "M",
+   	900 => "CM",
+   	500 => "D",
+   	400 => "CD",
+   	100 => "C",
+   	90 => "XC",
+   	50 => "L",
+   	40 => "XL",
+   	10 => "X",
+   	9 => "IX",
+   	5 => "V",
+   	4 => "IV",
+   	1 => "I"
+}
 
+def romanize(arabic_numeral)
+  #creating an empty string to push the roman numeral into then return that value 
+  roman = ""
+
+  number = arabic_numeral
+  # assigning a value for the number passed through so that we will be able to find the remainder too 
+
+  NUMBERS.each do | key, value |
+   	# running a each loop with two different parimeters, key is the number and value is what it will turn into 
+   	roman << value *(number / key)
+
+   	# were taking the number that was assigned and dividing it the closest key 
+   	# then timising the number of times it passes through the value and then added the total by the key 
+   	number = number % key
+  end
+
+return roman 
+# return the roman string 
 end
